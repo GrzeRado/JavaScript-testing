93fed039ca6e6e90648668b0ea62685261f5dc42
Grzegorz Radomski <rc36@wp.pl>
2018-03-01 04:05:05 +0100

    add do while loop

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-for (i=1; i<30; i++) {
+var i=0;
+do{
+    console.log(i);
+    i++
+}while (i<=11);
+
+
+/*for (var i=1; i<=30; i++) {
+    console.log(i);
+}
+if (i%2 === 0){
+    console.log("worked");
+}else if (i%3 === 0);
+    console.log("wrrrr")
+}else{
     console.log(i);
 }
 
